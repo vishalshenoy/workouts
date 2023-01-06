f1f24e51ae14380bf0c9cbbac801c3e5bb84d0dd
Vishal Shenoy <vishalshenoy@Vishals-MBP-2.lan>
2023-01-05 20:23:45 -0800

    reduce toks

--- pages/api/generate-response.js
+++ pages/api/generate-response.js
@@ -25,7 +25,7 @@ export default async function (req, res) {
       model: "text-davinci-003",
       prompt: prompt,
       temperature: 0.6,
-      max_tokens: 2048,
+      max_tokens: 1024,
     });
     res.status(200).json({ result: completion.data.choices[0].text });
   } catch (error) {
